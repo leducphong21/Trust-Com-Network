97ee63d4836e3f566e4ee5fd3090cee0dd013d4d
Phong Le <leducphong21@gmail.com>
2025-05-02 22:25:51 +0700

    midleware example

--- asset-transfer-basic/application-gateway-javascript/src/app.js
+++ asset-transfer-basic/application-gateway-javascript/src/app.js
@@ -71,7 +71,7 @@ async function main() {
         await readAssetByID(contract);
 
         // Cập nhật tài sản không tồn tại
-        await updateNonExistentAsset(contract);
+        // await updateNonExistentAsset(contract);
     } finally {
         await gateway.disconnect();
     }
