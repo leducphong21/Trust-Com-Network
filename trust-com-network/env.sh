2b49fbbd34b14f4292dfad37b3b973dc830027c7
Phong Le <leducphong21@gmail.com>
2025-05-07 22:57:16 +0700

    add config file

---
+++ trust-com-network/env.sh
@@ -0,0 +1,9 @@
+ENB_NETWOK_NAME=bank
+ENV_CLUSTER_NAME=bank-cluster
+ENV_DOMAIN=localho.st
+ENV_CHANNEL_NAME=mychannel
+ENV_ORG_NAMES="bank-org land-org"
+ENV_NUM_PEERS_PER_ORG=2
+ENV_ORDERER_NAME=orderer
+ENV_NUM_ORDERERS=3
+
