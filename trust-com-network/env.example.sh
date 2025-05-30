134787713fdf73d8589712d4bc3285378790b457
Phong Le <leducphong21@gmail.com>
2025-05-30 16:05:28 +0700

    fix bug load env

--- trust-com-network/env.example.sh
+++ trust-com-network/env.example.sh
@@ -1,9 +1,9 @@
 ENV_NETWOK_NAME=bank
 ENV_CLUSTER_NAME=bank-cluster
 ENV_DOMAIN=localho.st
+ENV_NS=bank
 ENV_CHANNEL_NAME=mychannel
-ENV_ORG_NAMES="bank-org land-org"
+ENV_ORG_NAMES="bank land"
 ENV_NUM_PEERS_PER_ORG=2
 ENV_ORDERER_NAME=orderer
 ENV_NUM_ORDERERS=3
-
